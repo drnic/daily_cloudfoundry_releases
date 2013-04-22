0618375287d347ff145f20da43373094ef57205b
Dr Nic Williams <drnicwilliams@gmail.com>
2013-04-21 21:13:02 -0700

    move rake test to integration

--- spec/integration/rake_release_spec.rb
+++ spec/integration/rake_release_spec.rb
@@ -2,4 +2,9 @@ describe "rake cf:release" do
   it "can be found" do
     `rake -T cf:release | grep cf:release`.strip.size.should > 0
   end
+  before do
+    # fail fast if no s3 credentials
+    # delete s3 blobstore bucket for tests
+    # recreate home
+  end
 end
